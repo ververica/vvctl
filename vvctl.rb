f6a0d6d3659c7d68656f5b8690106ce3b191d6f0
Hector Sanchez Pajares <34852+hecspc@users.noreply.github.com>
2025-06-25 10:23:51 +0200

    vvctl 2025.6.1-preview.18

--- vvctl.rb
+++ vvctl.rb
@@ -1,9 +1,9 @@
 class Vvctl < Formula
-  version "2025.6.1-preview.17"
+  version "2025.6.1-preview.18"
   desc "Your CLI tool description"
   homepage "https://app.ververica.cloud/"
-  url "https://github.com/ververica/vvctl/releases/download/2025.6.1-preview.17/vvctl-2025.6.1-preview.17-aarch64-apple-darwin.tar.gz"
-  sha256 "fae2c00ee27f69d2b78b286a077342e52ab98bddf1c0c6999352ae1c7a05e6db"  # This value must be updated automatically
+  url "https://github.com/ververica/vvctl/releases/download/2025.6.1-preview.18/vvctl-2025.6.1-preview.18-aarch64-apple-darwin.tar.gz"
+  sha256 "7d7da39c1f23f6c35558fb5d4f69ed06e53788316180cbea9a7cd2964a726f4e"  # This value must be updated automatically
   license "Your-License"
 
   def install
